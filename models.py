6ee847abd333bf4a28ae61833687128575ca13fb
adityassrana <adityassrana2@gmail.com>
2021-12-28 12:04:30 +0100

    fix lightning things and add config file

--- models.py
+++ models.py
@@ -1,7 +1,6 @@
 import torch
-from torch import nn
 import torch.nn.functional as F
-from torch import tensor
+from torch import nn
 
 
 class Binarizer(torch.autograd.Function):
--- train.py
+++ train.py
@@ -1,16 +1,24 @@
-import torch
-from torch import nn
-from torchvision import transforms
-from torch.utils.data import DataLoader,Dataset
-import pytorch_lightning as pl
-from torchvision.utils import make_grid
 import glob
-from PIL import Image
-from models import Encoder,Decoder
 from argparse import ArgumentParser
+from typing import Dict
+
+import pytorch_lightning as pl
+import torch
+from PIL import Image
+from torch import nn
+from torch.utils.data import DataLoader, Dataset
+from torchvision import transforms
+from torchvision.utils import make_grid
+
+from models import Decoder, Encoder
+
+from pydantic import BaseModel
+import yaml
+
 
 class ImageDataset(Dataset):
     """Dataset class for creating data pipeline"""
+
     def __init__(self, glob_pattern, patchsize):
         self.image_paths, self.patchsize = glob.glob(glob_pattern), patchsize
 
@@ -19,7 +27,7 @@ class ImageDataset(Dataset):
     def transform(self, image):
         if image.mode == 'L':
             image = image.convert('RGB')
-        self.data_transforms = transforms.Compose([transforms.RandomCrop(size = self.patchsize),
+        self.data_transforms = transforms.Compose([transforms.RandomCrop(size=self.patchsize),
                                                    transforms.RandomHorizontalFlip(),
                                                    transforms.RandomVerticalFlip(),
                                                    transforms.ToTensor()])
@@ -27,78 +35,88 @@ class ImageDataset(Dataset):
 
     def __getitem__(self, index):
         image = Image.open(self.image_paths[index])
-        image= self.transform(image)
+        image = self.transform(image)
         return image
 
+
+class Config(BaseModel):
+    train_files: str
+    val_files: str
+    image_size: int
+    lr: float
+    batch_size: int
+    num_epochs: int
+
+
 class AE(pl.core.LightningModule):
-    def __init__(self, **kwargs):
+    def __init__(self, config: Config):
         super().__init__()
-        
-        self.save_hyperparameters()
 
         self.encoder = Encoder()
         self.decoder = Decoder()
         self.loss_func = nn.MSELoss()
-        
-    def forward(self,x):
+
+        self.config: Config = config
+
+    def forward(self, x):
         return self.decoder(self.encoder(x))
-    
+
     def training_step(self, batch, batch_idx):
         x = batch
         x_hat = self(x)
         loss = self.loss_func(x, x_hat)
-        psnr = -10*torch.log10(loss)
-        return {'loss':loss,'psnr':psnr}
-    
-    def training_epoch_end(self,training_step_outputs):
+        return loss
+
+    def training_epoch_end(self, training_step_outputs):
         # [results, results, results]
-        avg_psnr = torch.stack([x['psnr'] for x in training_step_outputs]).mean()
         avg_loss = torch.stack([x['loss'] for x in training_step_outputs]).mean()
-        self.logger.experiment.add_scalar('loss/avg_train',avg_loss,self.current_epoch)
-        self.logger.experiment.add_scalar('psnr/train',avg_psnr,self.current_epoch)
-        return avg_loss
-    
+        avg_psnr = -10*torch.log10(avg_loss)
+        self.logger.experiment.add_scalar('loss/avg_train', avg_loss, self.current_epoch)
+        self.logger.experiment.add_scalar('psnr/train', avg_psnr, self.current_epoch)
+
     def validation_step(self, batch, batch_idx):
         x = batch
         x_hat = self(x)
         loss = self.loss_func(x, x_hat)
-        psnr = -10*torch.log10(loss)
-        return {'val_loss':loss,'psnr':psnr, 'x':x,'x_hat':x_hat}
-    
-    def validation_epoch_end(self,validation_step_outputs):
+        return {'val_loss': loss, 'x': x, 'x_hat': x_hat}
+
+    def validation_epoch_end(self, validation_step_outputs):
         # [results, results, results]
-        avg_psnr = torch.stack([x['psnr'] for x in validation_step_outputs]).mean()
         avg_loss = torch.stack([x['val_loss'] for x in validation_step_outputs]).mean()
+        avg_psnr = -10*torch.log10(avg_loss)
         x = validation_step_outputs[-1]['x']
         x_hat = validation_step_outputs[-1]['x_hat']
-        x_grid = make_grid(x,4)
+        x_grid = make_grid(x, 4)
         x_hat_grid = make_grid(x_hat, 4)
-        
+
         self.logger.experiment.add_image('val/original', x_grid, self.current_epoch)
-        self.logger.experiment.add_image('val/reconstructions',x_hat_grid,self.current_epoch)
-        self.logger.experiment.add_scalar('loss/avg_val',avg_loss,self.current_epoch)
-        self.logger.experiment.add_scalar('psnr/val',avg_psnr,self.current_epoch)
-        return {'val_loss':avg_loss}
-    
+        self.logger.experiment.add_image('val/reconstructions', x_hat_grid, self.current_epoch)
+        self.logger.experiment.add_scalar('loss/avg_val', avg_loss, self.current_epoch)
+        self.logger.experiment.add_scalar('psnr/val', avg_psnr, self.current_epoch)
+
     def train_dataloader(self):
-        train_ds = ImageDataset(self.hparams.train_glob, 128)
-        return DataLoader(train_ds, batch_size=16, shuffle=True, num_workers=4)
-    
+        train_ds = ImageDataset(self.config.train_files, self.config.image_size)
+        return DataLoader(train_ds, batch_size=self.config.batch_size, shuffle=True, num_workers=4)
+
     def val_dataloader(self):
-        valid_ds = ImageDataset(self.hparams.valid_glob, 128)
-        return DataLoader(valid_ds, batch_size=16, shuffle=False, num_workers=4)
-    
+        valid_ds = ImageDataset(self.config.val_files, self.config.image_size)
+        return DataLoader(valid_ds, batch_size=self.config.batch_size, shuffle=False, num_workers=4)
+
     def configure_optimizers(self):
-        return torch.optim.Adam(self.parameters(), lr=3e-4)
+        return torch.optim.Adam(self.parameters(), lr=self.config.lr)
 
 
 if __name__ == '__main__':
     parser = ArgumentParser()
-    parser.add_argument("--train_glob", default = "/home/adityassrana/datatmp/Datasets/CLIC/*png")
-    parser.add_argument("--valid_glob", default = "/home/adityassrana/datatmp/Datasets/kodak/*.png")
+    parser.add_argument("--config_path", default="config.yml")
     args = parser.parse_args()
-    
-    dict_args = vars(args)
-    model = AE(**dict_args)
-    trainer = pl.Trainer(gpus=1,max_epochs=5,check_val_every_n_epoch=1)
+
+    with open(args.config_path, "r") as stream:
+        config_dict: Dict = yaml.safe_load(stream)
+        config: Config = Config.parse_obj(config_dict)
+
+    model = AE(config)
+    trainer = pl.Trainer(gpus=1,
+                         max_epochs=config.num_epochs,
+                         check_val_every_n_epoch=1)
     trainer.fit(model)
